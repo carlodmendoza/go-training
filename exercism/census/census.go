48c351ccb1b14820b6929594e65d9a34ee3001c8
Carlo Mendoza <carlomendoza@cobenagroup.com>
2022-01-21 14:56:25 +0800

    add solution to Ex 23

---
+++ exercism/census/census.go
@@ -0,0 +1,41 @@
+// Package census simulates a system used to collect census data.
+package census
+
+// Resident represents a resident in this city.
+type Resident struct {
+	Name    string
+	Age     int
+	Address map[string]string
+}
+
+// NewResident registers a new resident in this city.
+func NewResident(name string, age int, address map[string]string) *Resident {
+	return &Resident{
+		Name:    name,
+		Age:     age,
+		Address: address,
+	}
+}
+
+// HasRequiredInfo determines if a given resident has all of the required information.
+func (r *Resident) HasRequiredInfo() bool {
+	return r.Name != "" && r.Address["street"] != ""
+}
+
+// Delete deletes a resident's information.
+func (r *Resident) Delete() {
+	r.Name = ""
+	r.Age = 0
+	r.Address = nil
+}
+
+// Count counts all residents that have provided the required information.
+func Count(residents []*Resident) int {
+	var count int
+	for _, res := range residents {
+		if res.HasRequiredInfo() {
+			count++
+		}
+	}
+	return count
+}
