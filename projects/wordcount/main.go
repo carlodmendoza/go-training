8c94a7419bf8f0a2480b0b5cca5120176af42116
Carlo Mendoza <carlomendoza@cobenagroup.com>
2022-01-28 11:47:07 +0800

    improve solution to HW#2

--- projects/wordcount/main.go
+++ projects/wordcount/main.go
@@ -8,6 +8,7 @@ import (
 	"sort"
 	"strings"
 	"sync"
+	"time"
 	"unicode"
 )
 
@@ -30,21 +31,23 @@ func (wc *WordCounter) Value(key string) int {
 
 func main() {
 	wc := WordCounter{counts: make(map[string]int)}
-	ch := make(chan []string)
-	var wordList []string
+	fileWordCountChan := make(chan map[string]int)
 	var keys []string
+
 	// process the files concurrently
 	for _, fileName := range os.Args[1:] {
-		go processFile(fileName, ch)
+		go countWordsInFile(fileName, fileWordCountChan)
 	}
-	// receive the list of words from channel then append to main list
+	// receive the word count of each file from the channel then count the words concurrently
 	for range os.Args[1:] {
-		wordList = append(wordList, <-ch...)
+		fileWordCount := <-fileWordCountChan
+		for k := range fileWordCount {
+			go wc.Inc(k)
+		}
 	}
-	// count the words concurrently
-	for _, word := range wordList {
-		go wc.Inc(word)
-	}
+	// delay before main routine prints the output
+	time.Sleep(time.Second)
+
 	// list the keys in alphabetical order then print it and its corresponding value
 	for k := range wc.counts {
 		keys = append(keys, k)
@@ -55,8 +58,13 @@ func main() {
 	}
 }
 
-func processFile(fileName string, ch chan []string) {
-	var words []string
+// countWordsInFile processes a file by getting all valid words from the file
+// then sending a map of the word counts to a map channel
+func countWordsInFile(fileName string, ch chan map[string]int) {
+	var counter int
+	wordCount := make(map[string]int)
+	wordChan := make(chan string)
+
 	// open file
 	file, err := os.Open(fileName)
 	if err != nil {
@@ -64,27 +72,37 @@ func processFile(fileName string, ch chan []string) {
 	}
 	defer file.Close()
 
-	// read file
+	// read file then get all words separated by spaces
 	scanner := bufio.NewScanner(file)
-	// get all words separated by spaces
 	scanner.Split(bufio.ScanWords)
-	// for each word, get only valid characters (letters or digits) then convert to lower case
+	// process each word concurrently
 	for scanner.Scan() {
-		var word string
-		for _, char := range scanner.Text() {
-			if unicode.IsDigit(char) || unicode.IsLetter(char) {
-				word += string(char)
-			}
-		}
-		word = strings.ToLower(word)
-		// append word to array if it's not an empty string
+		go processWord(scanner.Text(), wordChan)
+		counter++
+	}
+	// receive the new word from the channel then count it if it's not an empty string
+	for i := 0; i < counter; i++ {
+		word := <-wordChan
 		if word != "" {
-			words = append(words, word)
+			wordCount[word] += 1
 		}
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatalf("Failed to parse file: %s", err)
 	}
-	// send the word array to the channel
-	ch <- words
+
+	ch <- wordCount
+}
+
+// processWord removes non-letter and non-digit characters from a string
+// then sends the new string in lowercase to a string channel
+func processWord(oldWord string, ch chan string) {
+	var newWord string
+	for _, char := range oldWord {
+		if unicode.IsLetter(char) || unicode.IsDigit(char) {
+			newWord += string(char)
+		}
+	}
+	newWord = strings.ToLower(newWord)
+	ch <- newWord
 }
