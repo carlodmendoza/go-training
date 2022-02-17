db87910f76db524ccb26057d56bd51c137630d11
Carlo Mendoza <carlodgmendoza@gmail.com>
2022-02-17 11:10:34 +0800

    setup redis

---
+++ final-project/server/redis/redis.go
@@ -0,0 +1,13 @@
+package redis
+
+import "github.com/go-redis/redis/v8"
+
+var Client = NewClient()
+
+func NewClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
