c25ad6c7dc9f801a24afc46329c1a2621b1bfb09
Carlo Mendoza <carlodgmendoza@gmail.com>
2022-04-20 15:59:19 +0800

    WIP: Refactor file reading and writing

--- final-project/server/storage/filebased/user.go
+++ final-project/server/storage/filebased/user.go
@@ -6,7 +6,7 @@ func (fdb *FilebasedDB) CreateUser(username, password string) error {
 	fdb.UserMux.Lock()
 	defer func() {
 		fdb.UserMux.Unlock()
-		updateDatabase(fdb)
+		writeToFile(filePtr, fdb)
 	}()
 
 	fdb.NextUserID++
