2c9967ae57cfe6ca1c3c6a190d2d10d4dabeb868
Carlo Mendoza <carlodgmendoza@gmail.com>
2022-04-21 21:29:38 +0800

    WIP: Implement gochi as router - Add authenticator middleware for authorizing requests - Update auth and categories

--- final-project/server/internal/auth/auth.go
+++ final-project/server/internal/auth/auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"context"
 	"crypto/rand"
 	"encoding/hex"
 	"encoding/json"
@@ -9,6 +10,10 @@ import (
 	"net/http"
 	"server/storage"
 )
+
+type userCtx string
+
+const UserKey userCtx = "username"
 
 type AuthRequest struct {
 	Username string `json:"username"`
@@ -22,99 +27,86 @@ var (
 )
 
 // Signin handles a sign in request by a client.
-// Upon successful sign in, a generated token
-// is given as a cookie to client for authorizing
-// future requests.
+// Upon successful sign in, a generated token is given as a cookie to client for authorizing future requests.
 func Signin(db storage.Service, w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		var signinReq AuthRequest
+	var signinReq AuthRequest
 
-		err := json.NewDecoder(r.Body).Decode(&signinReq)
-		if err != nil {
-			fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if signinReq.Username == "" || signinReq.Password == "" {
-			fmt.Printf("Error in %s: %s\n", r.URL.Path, "Missing required fields.")
-			http.Error(w, "Missing required fields.", http.StatusBadRequest)
-			return
-		}
-
-		ok, err := db.AuthenticateUser(signinReq.Username, signinReq.Password)
-		if !ok {
-			http.Error(w, ErrInvalidLogin.Error(), http.StatusUnauthorized)
-			return
-		}
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		token := generateSessionToken()
-		err = db.CreateSession(signinReq.Username, token)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		http.SetCookie(w, &http.Cookie{
-			Name:  "Token",
-			Value: token,
-		})
-		_, _ = w.Write([]byte("Logged in successfully!"))
-	default:
-		http.Error(w, "", http.StatusMethodNotAllowed)
+	err := json.NewDecoder(r.Body).Decode(&signinReq)
+	if err != nil {
+		fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	if signinReq.Username == "" || signinReq.Password == "" {
+		fmt.Printf("Error in %s: %s\n", r.URL.Path, "Missing required fields.")
+		http.Error(w, "Missing required fields.", http.StatusBadRequest)
+		return
+	}
+
+	ok, err := db.AuthenticateUser(signinReq.Username, signinReq.Password)
+	if !ok {
+		http.Error(w, ErrInvalidLogin.Error(), http.StatusUnauthorized)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	token := generateSessionToken()
+	err = db.CreateSession(signinReq.Username, token)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.SetCookie(w, &http.Cookie{
+		Name:  "Token",
+		Value: token,
+	})
+	_, _ = w.Write([]byte("Logged in successfully!"))
 }
 
 // Signup handles a sign up request by a client.
 // It checks if an account already exists.
 func Signup(db storage.Service, w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		var signupReq AuthRequest
+	var signupReq AuthRequest
 
-		err := json.NewDecoder(r.Body).Decode(&signupReq)
-		if err != nil {
-			fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		if signupReq.Username == "" || signupReq.Password == "" {
-			fmt.Printf("Error in %s: %s\n", r.URL.Path, "Missing required fields.")
-			http.Error(w, "Missing required fields.", http.StatusBadRequest)
-			return
-		}
-
-		exists, err := db.UserExists(signupReq.Username)
-		if exists {
-			http.Error(w, ErrDuplicateUser.Error(), http.StatusConflict)
-			return
-		}
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		err = db.CreateUser(signupReq.Username, signupReq.Password)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		_, _ = w.Write([]byte("Signed up successfully!"))
-	default:
-		http.Error(w, "", http.StatusMethodNotAllowed)
+	err := json.NewDecoder(r.Body).Decode(&signupReq)
+	if err != nil {
+		fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
+
+	if signupReq.Username == "" || signupReq.Password == "" {
+		fmt.Printf("Error in %s: %s\n", r.URL.Path, "Missing required fields.")
+		http.Error(w, "Missing required fields.", http.StatusBadRequest)
+		return
+	}
+
+	exists, err := db.UserExists(signupReq.Username)
+	if exists {
+		http.Error(w, ErrDuplicateUser.Error(), http.StatusConflict)
+		return
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = db.CreateUser(signupReq.Username, signupReq.Password)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte("Signed up successfully!"))
 }
 
-// GenerateSessionToken returns a token for authorizing
-// client requests.
+// GenerateSessionToken returns a token for authorizing client requests.
 func generateSessionToken() string {
 	b := make([]byte, 16)
 	_, err := rand.Read(b)
@@ -124,25 +116,44 @@ func generateSessionToken() string {
 	return hex.EncodeToString(b)
 }
 
-// AuthenticateToken checks if token from a request cookie is associated
-// to an existing session. If yes, it returns the corresponding
-// username. If not, or no cookie is found, it returns an empty string.
-func AuthenticateToken(db storage.Service, w http.ResponseWriter, r *http.Request) string {
-	tokenCookie, err := r.Cookie("Token")
-	if err != nil {
-		fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
-		http.Error(w, ErrInvalidToken.Error(), http.StatusUnauthorized)
-		return ""
-	}
+// Authenticator is a middleware that checks if token from a request cookie is associated to an existing session.
+// If yes, it passes the username to the request context then proceeds with the request.
+// If not, or no cookie is found, an error response is sent.
+func Authenticator(db storage.Service) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		hfn := func(w http.ResponseWriter, r *http.Request) {
+			tokenCookie, err := r.Cookie("Token")
+			if err != nil {
+				fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
+				http.Error(w, ErrInvalidToken.Error(), http.StatusUnauthorized)
+				return
+			}
 
-	session, err := db.FindSession(tokenCookie.Value)
-	if session.Username == "" {
-		http.Error(w, ErrInvalidToken.Error(), http.StatusUnauthorized)
+			session, err := db.FindSession(tokenCookie.Value)
+			if session.Username == "" {
+				http.Error(w, ErrInvalidToken.Error(), http.StatusUnauthorized)
+				return
+			}
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			ctx := r.Context()
+			ctx = context.WithValue(ctx, UserKey, session.Username)
+
+			next.ServeHTTP(w, r.WithContext(ctx))
+		}
+		return http.HandlerFunc(hfn)
+	}
+}
+
+// GetUser returns the username from a request context using UserKey as key.
+func GetUser(r *http.Request) string {
+	switch v := r.Context().Value(UserKey).(type) {
+	case string:
+		return v
+	default:
 		return ""
 	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return ""
-	}
-	return session.Username
 }
--- final-project/server/internal/categories/categories.go
+++ final-project/server/internal/categories/categories.go
@@ -15,21 +15,16 @@ var (
 // ProcessCategories handles a categories/ request by a client.
 // The client can get all categories.
 func ProcessCategories(db storage.Service, w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		categories, err := db.GetCategories()
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	categories, err := db.GetCategories()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-		err = json.NewEncoder(w).Encode(categories)
-		if err != nil {
-			fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	default:
-		http.Error(w, "", http.StatusMethodNotAllowed)
+	err = json.NewEncoder(w).Encode(categories)
+	if err != nil {
+		fmt.Printf("Error in %s: %s\n", r.URL.Path, err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
